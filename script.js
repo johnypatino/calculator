0510b59d3f2a7072381bbe4e24206fc9b926d4d6
Jonathan Patino <johnypatino99@gmail.com>
2023-02-17 22:57:31 -0500

    added button functions

--- script.js
+++ script.js
@@ -19,5 +19,25 @@ var operate = function(operator, x, y) {
         default: break;
     }
 }
+const screen = document.querySelector('.ans');
+const button = document.querySelectorAll('.button');
+const operator = document.querySelectorAll('.operator');
+for (let i=0; i < button.length; i++ ) {
+   button[i].addEventListener('click', ()=> {
+    if (screen.textContent === '0') {
+        screen.textContent = '';
+    }
+        let display = button[i].textContent;
+        screen.textContent = screen.textContent + display;
+        console.log(button[i].textContent);
+    })
+}
+for (let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', ()=> {
+        let x = screen.textContent;
+        let op = operator[i].textContent;
+        console.log(op);
 
-console.log(operate('+',3,7));
+    })
+}
+
